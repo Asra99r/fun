7e701f212255e2078f0e8417b3adbf5963400f81
Asrar <139654678+Asra99r@users.noreply.github.com>
2023-07-30 21:28:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function TemprayName() {
+   return(
+   
+       <div>
+     
+      <nav className="logo-1">
+         <img src="https://th.bing.com/th/id/OIP.QfaRl_e60CVUnfw-sSHb1wHaGo?pid=ImgDet&rs=1"  /> <h3>ReactFacts</h3>
+        
+          <h4>React Cours - Projecr 1</h4>
+      </nav>
+      <main>
+       <h1>Fun fact about React</h1>
+      <ul>
+        <li>was frist released in 2013</li>
+        <li>was originally created by jorden</li>
+        <li>has well over 100k stars on GitHub</li>
+        <li>Powers thousands of enterprise</li>
+      </ul>
+   </main>
+   <footer>
+      <small>@2023 by Asrar devolopment</small>
+   </footer>
+   </div>
+  
+   )
+
+};
+ReactDOM.render(<TemprayName />, document.getElementById("root"));
